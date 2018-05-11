e0018e7e714dbd0e4a2a7af20950602b3d6c5577
JB_IrationV2 <jordan.bisanz@gmail.com>
2018-05-11 08:07:01 -0700

    v0.11

--- R/read_qza.R
+++ R/read_qza.R
@@ -12,6 +12,8 @@
 #' \item artifact$type - the semantic type of the object (ex FeatureData[Sequence])
 #' \item artifact$format - the format of the qiime artifact
 #' \item artifact$provenance - information tracking how the object was created
+#' \item artifact$contents - a table of all the files contained within the artifact and their file size
+#' \item artifact$version - the reported version for the artifact, a warning error may be thrown if a new version is seen
 #' }
 #' @export
 #'
@@ -28,10 +30,15 @@ untar(file, exdir=tmp)
 unpacked<-untar(file, exdir=tmp, list=T)
 
 artifact<-read_yaml(paste0(tmp,"/", grep(paste0(gsub("/..+","", unpacked[1]),"/metadata.yaml"), unpacked, value=T) )) #start by loading in the metadata not assuming it will be first file listed
+artifact$contents<-data.frame(files=unpacked)
+artifact$contents$size=sapply(paste0(tmp, "/", artifact$contents$files), file.size)
+artifact$version=read.table(paste0(tmp,"/",artifact$uuid, "/VERSION"))
 
-#get data dependent on type
+if(sum(artifact$version$V2==c("2","4","2018.4.0"))!=3){warning("It is unclear if current artifact version is supported, errors may occur in import...")}#check version and throw warning if new format
+
+  #get data dependent on type
 if(grepl("FeatureTable\\[[A-z]+\\]", artifact$type)){ #FeatureTable[Balance]|FeatureTable[Composition]|FeatureTable[Frequency]|FeatureTable[PresenceAbsence]|FeatureTable[RelativeFrequency]
-  artifact$data<-as(biom_data(read_biom(paste0(tmp, "/", artifact$uui,"/data/feature-table.biom"))),"matrix")
+  suppressWarnings(artifact$data<-as(biom_data(read_biom(paste0(tmp, "/", artifact$uui,"/data/feature-table.biom"))),"matrix")) #suppressing warning about \n characters
 } else if (grepl("Phylogeny\\[[A-z]+\\]", artifact$type)){ #Phylogeny[Rooted]|Phylogeny[Unrooted]
   artifact$data<-read.tree(paste0(tmp,"/",artifact$uuid,"/data/tree.nwk"))
 } else if (artifact$type=="DistanceMatrix") { #DistanceMatrix
@@ -55,6 +62,8 @@ if(grepl("FeatureTable\\[[A-z]+\\]", artifact$type)){ #FeatureTable[Balance]|Fea
   artifact$data$Vectors[,2:len]<-apply(artifact$data$Vectors[,2:len], 2, as.numeric)
 } else if (artifact$type=="FeatureData[Sequence]") {
   artifact$data<-readDNAStringSet(paste0(tmp,"/",artifact$uuid,"/data/dna-sequences.fasta"))
+} else if (artifact$type=="FeatureData[AlignedSequence]") {
+  artifact$data<-readDNAMultipleAlignment(paste0(tmp,"/",artifact$uuid,"/data/aligned-dna-sequences.fasta"))
 } else if (grepl("Sequences", artifact$type)) {
   artifact$data<-data.frame(files=list.files(paste0(tmp,"/", artifact$uuid,"/data")))
   artifact$data$size<-format(sapply(artifact$data$files, function(x){file.size(paste0(tmp,"/",artifact$uuid,"/data/",x))}, simplify = T))
